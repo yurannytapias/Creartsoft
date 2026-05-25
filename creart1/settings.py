581ee0961c2622a2d54a4adae54348ed61bd0d56
criss842 <cristinaisabelborjaacosta@gmail.com>
2026-05-25 15:38:35 -0500

    Usar sqlite en render

--- creart1/settings.py
+++ creart1/settings.py
@@ -72,18 +72,12 @@ WSGI_APPLICATION = 'creart1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'creart',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators

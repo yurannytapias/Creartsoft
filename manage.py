379b0ae0c850225f276aab3ef4e595ccf562773c
criss842 <cristinaisabelborjaacosta@gmail.com>
2026-06-16 22:30:33 -0500

    Fix: quitar pymysql de todos los archivos

--- manage.py
+++ manage.py
@@ -3,9 +3,6 @@
 import os
 import sys
 
-import pymysql
-pymysql.install_as_MySQLdb()
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'creart1.settings')

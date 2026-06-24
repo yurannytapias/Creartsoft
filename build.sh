67d8c368bf2e5a3f932534cc4a8dc5a8e3033933
criss842 <cristinaisabelborjaacosta@gmail.com>
2026-06-24 17:07:27 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py limpiar_prueba
+python manage.py generar_solicitudes_transacciones

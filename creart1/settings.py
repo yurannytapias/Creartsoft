f185c85e49e2eec250789fa54ed49faef782670c
criss842 <cristinaisabelborjaacosta@gmail.com>
2026-06-24 17:53:23 -0500

    Integrar Cloudinary para almacenamiento permanente de imagenes

--- creart1/settings.py
+++ creart1/settings.py
@@ -20,8 +20,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'creart',
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # ===================== MIDDLEWARE =====================
 MIDDLEWARE = [

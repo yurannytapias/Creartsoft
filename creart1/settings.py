bd1c9c96778bacb8bcd6708dce00e0070710a298
criss842 <cristinaisabelborjaacosta@gmail.com>
2026-06-24 19:05:08 -0500

    Fix: quitar MEDIA_ROOT para usar Cloudinary correctamente

--- creart1/settings.py
+++ creart1/settings.py
@@ -94,7 +94,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ===================== ARCHIVOS MEDIA =====================
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
